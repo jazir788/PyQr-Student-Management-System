537a898a32130593c4a7a2863554ad11da209ff0
jazir788 <jazirsx5@gmail.com>
2023-11-30 15:29:40 +0000

    Populated table in application with data from databas

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from PyQt6.QtWidgets import QApplication, QLabel, QWidget, QGridLayout, \
-     QLineEdit,QPushButton, QComboBox, QMainWindow, QTableWidget
+     QLineEdit,QPushButton, QComboBox, QMainWindow, QTableWidget,QTableWidgetItem
 from PyQt6.QtGui import QAction
 import sys
+import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -22,13 +23,22 @@ class MainWindow(QMainWindow):
         self.table.setHorizontalHeaderLabels(("Id", "Name","Course","Mobile"))
         self.setCentralWidget(self.table)
 
-    def load_data(self):
-        self.table
 
+
+    def load_data(self):
+        connection = sqlite3.connect("database.db")
+        results  = connection.execute("SELECT * FROM students")
+        self.table.setRowCount(0)
+        for row_number, row_data in enumerate(results):
+            self.table.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.table.setItem(row_number, column_number, QTableWidgetItem(str(data)))
+        connection.close()
 
 
 app = QApplication(sys.argv)
 speed_calculator = MainWindow()
+speed_calculator.load_data()
 speed_calculator.show()
 sys.exit(app.exec())
 
